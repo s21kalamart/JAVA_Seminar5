fcfc591cb4ec952ca6bec7082d5260d87449ab85
localuser <localuser@VEA.LV>
2023-05-12 15:56:20 +0300

    Edit: change service related to repo

--- src/main/java/lv/venta/repos/IProductRepo.java
+++ src/main/java/lv/venta/repos/IProductRepo.java
@@ -5,5 +5,10 @@ import org.springframework.data.repository.CrudRepository;
 import lv.venta.model.Product;
 
 public interface IProductRepo extends CrudRepository<Product, Long> {
-
+	//it will generate query with where title = XXX and description = XXX and price = XXX	
+	//it will verify if the product is already in DB
+	boolean existsByTitleAndDescriptionAndPrice(String inputTitle, String inputDescription, float inputPrice);
+	
+	//it will return the product from DB
+	Product findByTitleAndDescriptionAndPrice(String inputTitle, String inputDescription, float inputPrice);
 }
